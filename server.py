d1b2c3b8eb0d0cf7ad1666cc9372b575279e560c
picasso250 <cumt.xiaochi@gmail.com>
2014-11-18 15:59:57 +0800

    server python

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-import SocketServer
+import socketserver
+import json
+import struct
+import logging
 
-class FileHandler(SocketServer.BaseRequestHandler):
+class FileHandler(socketserver.BaseRequestHandler):
     """
     The RequestHandler class for our server.
 
@@ -11,26 +14,49 @@ class FileHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
+        head = self.get_request_head()
+        cmd = head['cmd']
+        if cmd == 'save file':
+            i = head['id']
+            p = pairs[i]['root_server']+head['filename']
+            logging.info("recv file %s", head['filename'])
+            self.save_file(p, head['data-size'])
+            self.request.sendall('save to {}'.format(p).encode())
+            print ("{} send {}, we save it to {}".format(self.client_address[0], head['filename'], p))
+
+    def get_request_head(self):
         hsize = self.request.recv(4)
         l, = struct.unpack('i', hsize)
         j = self.request.recv(l)
-        head = json.loads(j)
-        
+        head = json.loads(j.decode())
+        return head
 
-
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+    def save_file(self, fname, total):
+        if total == 0:
+            with open(fname, 'a') as f:
+                os.utime(fname, None)
+                return
+        chunk_size = 1024
+        tmp = fname+'.xc.tmp'
+        with open(tmp, 'w') as f:
+            while total > 0:
+                f.write(self.request.recv(chunk_size))
+                total -= chunk_size
+        os.rename(tmp, fname)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.stderr.write('Usage: sys.argv[0] <port>')
-        sys.exit(1)
-    HOST, PORT = "localhost", int(sys.argv[1])
+
+    logging.basicConfig(filename='sync.log')
+
+    config = json.load(open('config.default.json'))
+    config.update(json.load(open('config.user.json')))
+    pairs = config['pairs']
 
     # Create the server, binding to localhost on port specified
-    server = SocketServer.TCPServer((HOST, PORT), FileHandler)
+    host = config['host']
+    port = config['port']
+    print('listen on {}:{}'.format(host, port))
+    server = socketserver.TCPServer((host, port), FileHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
