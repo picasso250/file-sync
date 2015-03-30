977e0c90ac5789e64d2c888945739c1c983788f5
picasso250 <cumt.xiaochi@gmail.com>
2015-03-30 18:05:20 +0800

    modify_time

--- http_client.php
+++ http_client.php
@@ -6,13 +6,13 @@ set_time_limit(0);
 
 $config = get_config();
 
-$url = $config['url'];
 $pairs = $config['pairs'];
 
 $interval = 1;
 $sleep = 0;
 while (true) {
     foreach ($pairs as $id => $pair) {
+        $url = $pair['url'];
         $root = $pair['root_client'];
         $ignore = $pair['ignore'];
         $changed = http_watch_dir($url, $root, $pair['root_server'], $ignore);
--- lib/lib_file.php
+++ lib/lib_file.php
@@ -23,7 +23,7 @@ function is_text_file($filename)
  */
 function load_modify_time()
 {
-    $f = __DIR__.'/modify_time'.$GLOBALS['id'];
+    $f = modify_time_file();
     if (is_file($f)) {
         return json_decode(file_get_contents($f), true) ?: array();
     }
@@ -37,6 +37,12 @@ function load_modify_time()
  */
 function save_modify_time($modify_table)
 {
-    $f = __DIR__.'/modify_time'.$GLOBALS['id'];
+    $f = modify_time_file();
     return file_put_contents($f, json_encode($modify_table));
+}
+
+function modify_time_file()
+{
+    $f = __DIR__.'/modify_time_'.sha1(serialize($GLOBALS['config']));
+    return $f;
 }
